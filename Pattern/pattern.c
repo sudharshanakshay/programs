dff31741964dbdef7d3ba4301491b28dd56d7fb3
sudharshan <sudharshan6acharya@gmail.com>
2022-06-09 17:26:24 +0530

    pattern

---
+++ Pattern/pattern.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void main(){
+
+int n;
+scanf("%d", &n);
+
+for(int i=1; i<=n; i++){
+	for(int j=0; j<n-i; j++){
+		printf(" ");
+	}
+	for(int j=1; j<=i; j++){
+		printf("%d",j);
+	}
+	for(int j=i-1; j>=1; j--){
+		printf("%d",j);
+	}
+	printf("\n");
+}
+
+for(int i=n-1; i>=1; i--){
+	for(int j=0; j<n-i; j++)
+	{
+		printf(" ");
+	}
+	for(int j=1; j<=i; j++)
+	{
+		printf("%d", j);
+	}
+	for(int j=i-1; j>=1; j--)
+	{
+		printf("%d", j);
+	}
+	printf("\n");
+}
+}
